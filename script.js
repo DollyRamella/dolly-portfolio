0d6648092204021e9ceec0483c8482df97a8ff77
DollyRamella <108594195+DollyRamella@users.noreply.github.com>
2026-06-13 15:26:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-// Ajouter la classe reveal à toutes les sections
 document.querySelectorAll("section").forEach(section => {
-    section.classList.add("reveal");
+  section.classList.add("reveal");
 });
 
-// Activer les sections visibles au scroll
-const reveals = document.querySelectorAll("section");
-
 window.addEventListener("scroll", () => {
-    reveals.forEach(section => {
-        const top = section.getBoundingClientRect().top;
-        if (top < window.innerHeight - 100) {
-            section.classList.add("active");
-        }
-    });
+  document.querySelectorAll("section").forEach(section => {
+    const top = section.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      section.classList.add("active");
+    }
+  });
 });
